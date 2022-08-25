e2eaf27ae3e6d8cb9f9fc1ebab638298dd685bab
Manvendra Singh Rathore <manvendrasrathore20015@gmail.com>
2022-08-25 17:57:58 +0530

    1st update

--- 5.2 NodeJs/index.js
+++ 5.2 NodeJs/index.js
@@ -1,29 +1,72 @@
 const args = process.argv.slice(2);
+let flag=0; // for cases input ...
+if(args[0]=="operation") { 
+    if(args[1]==='addition' && args[3]!= undefined) {
+        flag=1;
+        let sum=0;
+        for(let i=2;i<args.length; i++) 
+            sum+= Number(args[i]);
+        if(!IntegerCheck(sum))
+            console.log(sum)
+        else
+            return;
+    }
+    else if(args[1] === 'multiply'&& args[3]!= undefined) {
+        flag=1;
+        let multi=1;
+        for(let i=2;i<args.length; i++) 
+            multi*= Number(args[i]);
+        if(!IntegerCheck(multi))
+            console.log(multi)
+        else
+            return;
 
-if(args[1] === 'addition') {
-    let sum=0;
-    for(let i=2;i<args.length; i++) 
-        sum+= Number(args[i]);
-    console.log(sum);
-}
-else if(args[1] === 'multiply') {
-    let multi=1;
-    for(let i=2;i<args.length; i++) 
-        multi*= Number(args[i]);
-    console.log(multi);
-}
-else if(args[1] === 'subtraction') {
-    if(args.length==4)
-        console.log(args[2]-args[3]);
-    else 
+    }
+    else if(args[1] === 'subtraction') {
+        flag=1;
+        if((args.length==4))
+        {
+            if(!IntegerCheck(args[2]-args[3]))
+                console.log(args[2]-args[3]);
+            else
+                return;
+        }
+        else 
+            console.log('Invalid Case !! - 2 inputs required ..');
+    }
+    else if(args[1] === 'division') {
+        flag=1;
+        if(args.length==4) {
+            n = +args[3];
+            if (!n) {  // Matches +0, -0, NaN
+                console.log('Invalid dividend ' + n);
+                }
+            else {
+                if(!IntegerCheck(args[2]/args[3])) 
+                        console.log(args[2]/args[3]);
+                else
+                    return;
+            }
+        }
+        else 
+            console.log('Invalid Case !! - 2 inputs required ..');
+    }
+    else {
         console.log('Invalid Case !!');
+        if(args[3]==undefined)
+            console.log("More than 1 input required");
+        }
+        if(!flag)
+            console.log("case must be either 'addition', 'subtraction', 'divide' or 'multiply'")
+        
+
+    function IntegerCheck(num) {
+        if(isNaN(num)) {
+            console.log("Invalid Case :- all inputs must be number");
+            return true;
+        }
+        return false;
+    }
 }
-else if(args[1] === 'division') {
-    if(args.length==4)
-        console.log(args[2]/ args[3]);
-    else 
-        console.log('Invalid Case !!');
-}
-else {
-    console.log('Invalid Case !!');
-}
+else   
+    console.log("1st input must be 'operation' ..");
--- 5.2 NodeJs/readme.md
+++ 5.2 NodeJs/readme.md
@@ -4,13 +4,13 @@
 
 1. Create a node.js command line program which reads operation(add, subtract, multiply, division) and values as arguments and returns the appropriate output onto console as in below criteria
 
-  a. node app.js --operation addition 1 2 3 4 => 1+2+3+4 = 10
-  
-  b. node app.js --operation multiply 1 2 3 4 => 1*2*3*4 = 24
+a. node app.js --operation addition 1 2 3 4 => 1+2+3+4 = 10
 
-  c. node app.js --operation subtraction 2 1 => 2 - 1 = 1
+b. node app.js --operation multiply 1 2 3 4 => 1*2*3*4 = 24
 
-  d. node app.js --operation division 4 2 => 4/2 = 2
+c. node app.js --operation subtraction 2 1 => 2 - 1 = 1
+
+d. node app.js --operation division 4 2 => 4/2 = 2
 
 2. All the invalid cases need to be handled for all the operations
 
